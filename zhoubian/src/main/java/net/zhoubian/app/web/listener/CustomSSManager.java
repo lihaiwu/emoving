70b9784a4190e909c1a61d18e86220e2c163e1ae
Edwin Li <lihaiwu@yahoo.com.cn>
2010-08-21 14:57:30 +0000

    更改bug

--- zhoubian/src/main/java/net/zhoubian/app/web/listener/CustomSSManager.java
+++ zhoubian/src/main/java/net/zhoubian/app/web/listener/CustomSSManager.java
@@ -12,6 +12,7 @@ import org.directwebremoting.WebContextFactory;
 import org.directwebremoting.event.ScriptSessionEvent;
 import org.directwebremoting.event.ScriptSessionListener;
 import org.directwebremoting.extend.RealScriptSession;
+import org.directwebremoting.impl.DefaultScriptSession;
 import org.directwebremoting.impl.DefaultScriptSessionManager;
 
 public class CustomSSManager extends DefaultScriptSessionManager implements org.directwebremoting.extend.InitializingBean {
@@ -35,25 +36,20 @@ public class CustomSSManager extends DefaultScriptSessionManager implements org.
 				System.out.println("httpSession:" + httpSession.getId());
 				User user = (User) httpSession.getAttribute("user");
 				
-				if (user == null) {
-					scriptSession.invalidate();
-					httpSession.invalidate();
-					return;
-				}
-				System.out.println("user:" + user.getUid());
+//				if (user == null) {
+//					scriptSession.invalidate();
+//					httpSession.invalidate();
+//					return;
+//				}
+				System.out.println("user:" + user);
 				String ssId = (String) httpSession.getAttribute(SS_ID);
 				System.out.println("ssId:" + ssId);
 				if (ssId != null) {
-					Collection<RealScriptSession> col= CustomSSManager.this.getScriptSessionsByHttpSessionId(httpSession.getId());
-					for(RealScriptSession old : col){
-						System.out.println("old:" + old.getId());
-						if(old.getId().equals(ssId)){
-							old.invalidate();
-						}
-					}
+					DefaultScriptSession old=sessionMap.get(ssId);
+                    if(old!=null)CustomSSManager.this.invalidate(old);
 				}
 				httpSession.setAttribute(SS_ID, scriptSession.getId());
-				scriptSession.setAttribute("uid", user.getUid());// 此处将uid和scriptSession绑定
+//				scriptSession.setAttribute("uid", user.getUid());// 此处将uid和scriptSession绑定
 			}
 
 			public void sessionDestroyed(ScriptSessionEvent event) {
